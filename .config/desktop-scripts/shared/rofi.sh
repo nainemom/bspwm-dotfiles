39c2db6a48f3289bd579e891e544dc820663d4a5
Amir Momenian <nainemom@gmail.com>
2022-05-02 00:55:47 +0430

    add support for highlighted row

--- .config/desktop-scripts/shared/rofi.sh
+++ .config/desktop-scripts/shared/rofi.sh
@@ -7,5 +7,21 @@ rofi_sudo() {
 }
 
 rofi_ask() {
-    echo -e "$1" | tr -s "\n" | rofi -dmenu -no-fixed-num-lines -p 'Select Action'
+    selected_row=0
+    selected_text=$2
+    options=$1
+    if [[ ! -z "$selected_text" ]]; then
+        IFSBAK=$IFS
+        IFS=$'\n'
+        for line in $options
+        do
+            if [[ "$line" == "$selected_text" ]]; then
+                break
+            else
+                ((selected_row++))
+            fi
+        done
+        IFS=$IFSBAK
+    fi
+    echo -e "$options" | tr -s "\n" | rofi -dmenu -no-fixed-num-lines -p "Select Action" -selected-row $selected_row
 }
